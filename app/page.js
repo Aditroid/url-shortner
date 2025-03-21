c85d97430a915a884813ec2f633098ce5add95ee
Aditroid <bora.aditya786@gmail.com>
2025-03-21 12:51:37 +0530

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import localFont from "next/font/local";
+import Link from "next/link";
+
+
+
+const poppins = localFont({
+  src: "./fonts/Poppins-ExtraBold.ttf",
+  variable: "--font-poppins",
+  weight: "100 900",
+});
+
+export default function Home() {
+  return (
+    <main>
+      <section className="flex flex-col">
+        <div className=" flex justify-start relative">
+          <Image  alt="an Image of a vector" src={"/vector.svg"} width={500} height={500} className="mx-auto mt-20 mb-15" priority  />
+        </div>
+        <div className="flex flex-col gap-5 items-center justify-center">
+          <p className={`text-3xl font-bold ${poppins.className}`}>
+            The best URL Shortner
+          </p>
+          <p className=" text-center lg:w-[40%] md:w-[60%]  sm:w-[70%]">
+          We are the world's most straightforward URL shortener. Unlike many other services that track users or require account registration, our platform prioritizes simplicity and privacy. We understand your needs, which is why we’ve designed a hassle-free URL shortener that respects your privacy while delivering a seamless experience.
+          </p>
+          <div className='flex gap-3 justify-start'>
+          <Link href="/shorten"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold text-white'>Try Now</button></Link>
+          <Link href="/github"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold text-white'>GitHub</button></Link>
+        </div>
+        </div>
+
+      </section>
+    </main>
+  );
+}
